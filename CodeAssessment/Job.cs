28dcdd4387c5794146e38d1aa3f2dd0820780d1e
Young Wang <yang.wang.nz@gmail.com>
2021-09-29 09:37:42 +1300

    Implement the job flow

--- CodeAssessment/Job.cs
+++ CodeAssessment/Job.cs
@@ -6,13 +6,48 @@ namespace CodeAssessment
     {
         private readonly string _fileName;
 
+        private int _valueRead { get; set; }
+        private int _valueFromConsole { get; set; }
+        private int _valueToWrite { get; set; }
+
         public Job(string readFilePath)
         {
             _fileName = readFilePath;
         }
         public void DoJob()
         {
-            // Do task here...
+            // 1. Read a number from file
+            ReadFile();
+            // 2. Read a number from console
+            ReadFromConsole();
+            // 3. Calculate addition
+            Calculate();
+            // 4. Write total into the file which is the reading file.
+            WriteFile();
+        }
+
+        private void ReadFile()
+        {
+
+        }
+
+        private void ReadFromConsole()
+        {
+
+        }
+
+        /// <summary>
+        /// This method can be overrided.
+        /// This is to do addition
+        /// </summary>
+        protected virtual void Calculate()
+        {
+
+        }
+
+        private void WriteFile()
+        {
+
         }
     }
 }
