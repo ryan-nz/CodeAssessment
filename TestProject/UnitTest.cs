e3d9b1fa04f536edbd3b7c895603e9e0fe7ee4bf
Young Wang <yang.wang.nz@gmail.com>
2021-09-29 09:22:01 +1300

    Add DoJob_NoReadFile test method

--- TestProject/UnitTest.cs
+++ TestProject/UnitTest.cs
@@ -1,14 +1,37 @@
+using CodeAssessment;
+using FluentAssertions;
 using System;
+using System.IO;
 using Xunit;
 
 namespace TestProject
 {
     public class UnitTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly string _fileName = @"c:\temp\test-unit-test.txt";
 
+        [Fact]
+        public void DoJob_NoReadFile()
+        {
+            // Arrange ...
+            // make sure the file not exists
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+
+            string textExpected = "123";
+            // Mock Console read
+            var input = new StringReader(textExpected);
+            Console.SetIn(input);
+
+            // Act ...
+            IJob job = new Job(_fileName);
+            job.DoJob();
+
+            // Assert ...
+            string textRead = File.ReadAllText(_fileName);
+            textRead.Should().Be(textExpected);
         }
     }
 }
